c541e7d288f9f78a3a787f7ec43cc99790213401
Clayrisee <ardikatamah@gmail.com>
2023-05-06 01:00:35 +0000

    add constant knowledge base

---
+++ alize/common/constant.py
@@ -0,0 +1,8 @@
+class BaseKnowledges:
+    LIST_KNOWLEDGES = [
+        "Iphone 14 Pro and Iphone 14 Promax Specification, Price",
+        "Best Phone in the world",
+        "Samsung S22 Specification and Price",
+        "Best Family Car in Indonesia",
+    ]
+    IDENTITY = "Alize is a chatbot to answer your question >_<)b"
